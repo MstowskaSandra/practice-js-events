96a42a2dda463101f12973b08d7a789b3d7dfcc5
Sandra <sandra.mstowskaa@gmail.com>
2025-08-28 14:28:54 +0200

    complete task 4.2

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,20 @@
 const elements = document.querySelectorAll('div');
 
 const addClass = function(e) {
+    e.stopPropagation();
     e.currentTarget.classList.add('clicked');
 }
 
 elements.forEach( function(item) {
         item.addEventListener('click', addClass);
 });
+
+const body = document.querySelector('body');
+
+const removeClass = function(e) {
+    elements.forEach(div => div.classList.remove('clicked'));
+}
+
+if(body) {
+    body.addEventListener('click', removeClass);
+}
